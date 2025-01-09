4bcdc8f37b5e695c3847eb686381be639ac7cacb
vitorfdrangel <vitorfdrangel@gmail.com>
2025-01-09 13:16:24 -0300

    Ajustes Cardapio page

--- frontend/src/routes/Cardapio.jsx
+++ frontend/src/routes/Cardapio.jsx
@@ -2,14 +2,9 @@
 import Navbar from "../components/Navbar.jsx";
 import ProdVitrine from "../components/ProdVitrine.jsx";
 
-// hooks
-import { useState } from "react";
-
 import classes from "./Cardapio.module.css";
 
 const Cardapio = () => {
-  const [filter, setFilter] = useState("todos");
-
   return (
     <>
       <Navbar showMenu={true} />
@@ -19,34 +14,8 @@ const Cardapio = () => {
           Clique em adicionar para colocar os produtos na sacola de compras. Se
           preferir, você pode pedir pelo nosso WhatsApp: (21) 98123-3465
         </p>
-        <div className={classes.filter_container}>
-          <button
-            onClick={() => setFilter("todos")}
-            className={filter === "todos" ? classes.target : ""}
-          >
-            Todos
-          </button>
-          <button
-            onClick={() => setFilter("BG")}
-            className={filter === "BG" ? classes.target : ""}
-          >
-            Hambúrgueres
-          </button>
-          <button
-            onClick={() => setFilter("AC")}
-            className={filter === "AC" ? classes.target : ""}
-          >
-            Acompanhamentos
-          </button>
-          <button
-            onClick={() => setFilter("DK")}
-            className={filter === "DK" ? classes.target : ""}
-          >
-            Bebidas
-          </button>
-        </div>
       </div>
-      <ProdVitrine filter={filter} />
+      <ProdVitrine />
     </>
   );
 };
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -5,6 +5,10 @@ import Navbar from "../components/Navbar.jsx";
 import classes from "./Home.module.css";
 
 const Home = () => {
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <Navbar showMenu={true} />
@@ -14,6 +18,7 @@ const Home = () => {
           <h1>Mais Pedidos</h1>
           <p>Lista de produtos mais pedidos</p>
         </div>
+        {<button onClick={handleClick}>Subir</button>}
       </div>
     </>
   );
