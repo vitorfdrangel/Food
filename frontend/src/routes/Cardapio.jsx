ddebba5233003e09f87bcbc191164358ab2d5a9d
Vitor Rangel <vitorfdrangel@gmail.com>
2025-07-01 18:30:34 -0300

    Refactor: menu page

--- frontend/src/routes/Cardapio.jsx
+++ frontend/src/routes/Cardapio.jsx
@@ -14,7 +14,7 @@ const Cardapio = () => {
         <h1 className={classes.title}>Nosso Cardápio</h1>
         <p className={classes.subtitle}>
           Clique em adicionar para colocar os produtos na sacola de compras. Se
-          preferir, você pode pedir pelo nosso WhatsApp: (21) 98123-3465
+          preferir, você pode pedir pelo nosso WhatsApp: (21) 99999-9999
         </p>
       </div>
       <ProdVitrine />
